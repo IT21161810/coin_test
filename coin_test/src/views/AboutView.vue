7668086882a97ab5cf96e4a9e3f04a15716ca4ac
Vikum Manchanayake <vikumviraj@vizuamatix.com>
2024-09-02 21:54:40 +0530

    initial commit

---
+++ coin_test/src/views/AboutView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="about">
+    <v-btn style="background-color: blue;
+    padding: 2rem;border-radius: 2px;" @click="onSubmit">SayHello</v-btn>
+    <br/>
+    <h2></h2>
+  </div>
+</template>
+
+
+<script setup> 
+import axios from 'axios';
+
+const onSubmit = async() => {
+  try{
+    const response = await axios.get("http://localhost:8085/api/proxy/hello")
+    
+  }catch(err){
+    console.log(err)
+  }
+}
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
